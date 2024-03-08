5af824c2486a183587d5d49f80291ae4698c613c
powxpow <powxpow@example.com>
2024-03-08 14:26:08 -0700

    updated date handling for both html and json, generalized count df creation

--- src/classes/whdata.py
+++ src/classes/whdata.py
@@ -15,7 +15,7 @@ import logging as log
 from pathlib import Path
 from zipfile import ZipFile
 #modules
-from dateutil import parser as dateutil_parser
+from dateutil import tz, parser as dateutil_parser
 from htmlement import parse as html_parse
 from pandas import DataFrame
 
@@ -103,11 +103,10 @@ class WatchHistoryDataHandler():
                     video_title=rec.get('title').replace('Watched ',''),
                     video_url=vd_url,
                     video_id=vd_id,
-                    view=dateutil_parser.isoparse(rec.pop('time'))
+                    view=dateutil_parser.isoparse(rec.pop('time')).astimezone(None)
                 )
                 views.append(view_record)
             else:
-                #print(rec)
                 survey_count += 1
 
         if len(views) > 0:
@@ -128,18 +127,23 @@ class WatchHistoryDataHandler():
     #       content-cell
     #           b "Products" (followed by "YouTube")
     #           optional b "Details" followed by "From Google Ads" (ads and surveys)
+    #                       I have encountered an old ad WITHOUT this section
     #           b "Why is this here?" followed by explanation and 1 a link
     @staticmethod
     def create_views_df_html(doc):
         '''
         create_views_df_html
         '''
+        tzinfos = {"EST": tz.gettz('US/Eastern'),
+            "CST": tz.gettz('US/Central'),
+            "MST": tz.gettz('US/Mountain'),
+            "PST": tz.gettz('US/Pacific')}
+
         views_df = None
         views = []
         idx = 0
-        root = html_parse(doc, encoding="UTF-8")
 
-        for outer_cell in root.iterfind(".//div[@class]"):
+        for outer_cell in html_parse(doc, encoding="UTF-8").iterfind(".//div[@class]"):
             if outer_cell.get('class').startswith('outer-cell'):
                 idx += 1
                 div = outer_cell.find('.//div[1]/div[@class][2]')
@@ -161,7 +165,7 @@ class WatchHistoryDataHandler():
                         video_title=video_alink.text,
                         video_url=vd_url,
                         video_id=vd_id,
-                        view=dateutil_parser.parse(view_date, fuzzy=True)
+                        view=dateutil_parser.parse(view_date, tzinfos=tzinfos, fuzzy=True)
                     )
                     views.append(view_record)
 
@@ -172,37 +176,38 @@ class WatchHistoryDataHandler():
             log.info('%7d views', views_df.shape[0])
         return views_df
 
-    @staticmethod
-    def create_videos_df(views_df):
+    def create_videos_df(self, views_df):
         '''
         Create Videos DataFrame from Views DataFrame.
-        Grab the Channel and Videos columns, drop duplicates.
-        Then count the number views and and add the count as a column.
-
         '''
-        videos_df = DataFrame(
-            views_df,
-            columns=['channel_id', 'channel_title', 'channel_url',
-                     'video_id', 'video_title', 'video_url']).drop_duplicates()
         # video_url is more unique than video_id
         # to get the right counts 'music.youtube' needs to be counted separately from 'www.youtube'
-        videos_df.loc[:, 'views'] = videos_df.loc[:, 'video_url'].map(
-            views_df.loc[:, 'video_url'].value_counts())
-        videos_df = videos_df.sort_values(by='views', ascending=False)
+        videos_df = self.create_count_df(
+            views_df, ['channel_id', 'channel_title', 'channel_url',
+                        'video_id', 'video_title', 'video_url'],
+                        'video_url', 'views')
         return videos_df
 
-    @staticmethod
-    def create_channels_df(videos_df):
+    def create_channels_df(self, videos_df):
         '''
         Create Channels DataFrame from Videos DataFrame.
-        Grab the Channel columns, drop duplicates.
-        Then count the number videos and and add the count as a column.
-
         '''
-        channels_df = DataFrame(
-            videos_df,
-            columns=['channel_id', 'channel_title', 'channel_url']).drop_duplicates()
-        channels_df.loc[:, 'videos'] = channels_df.loc[:, 'channel_url'].map(
-            videos_df['channel_url'].value_counts())
-        channels_df = channels_df.sort_values(by='videos', ascending=False)
+        channels_df = self.create_count_df(
+            videos_df, ['channel_id', 'channel_title', 'channel_url'],
+            'channel_url', 'videos')
         return channels_df
+
+    @staticmethod
+    def create_count_df(a_df, cols, key, count_name):
+        '''
+        Generic making a DataFrame into one with a count column.
+        Grab the included columns, drop duplicates.
+        Then count the key column and and add the count as a count_name.
+        For example: create a "channels" DataFrame, and add the count column
+        "videos".
+        '''
+        count_df = DataFrame(a_df,columns=cols).drop_duplicates()
+        count_df.loc[:, count_name] = count_df.loc[:, key].map(
+            a_df[key].value_counts())
+        count_df = count_df.sort_values(by=count_name, ascending=False)
+        return count_df
